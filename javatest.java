39460cf7f07625f66cd0a95aac7b2c5993d2972e
gakigakigakky <mizumizuswim@gmail.com>
2024-08-14 01:25:48 +0900

    add javatest.java

---
+++ javatest.java
@@ -0,0 +1,27 @@
+package jp.co.f1.superintro.ch04;
+ 
+public class Variable {
+ 
+    public static void main(String[] args) {
+ 
+        //タイトル
+        String title;
+        title = "Java超入門";
+ 
+        //価格
+        int price;
+        price = 3000;
+ 
+        System.out.print("本のタイトルは");
+        System.out.print(title);
+        System.out.println("です。");
+ 
+        System.out.println();
+ 
+        System.out.print("本の価格は");
+        System.out.print(price);
+        System.out.println("円です。");
+ 
+    }
+ 
+}
